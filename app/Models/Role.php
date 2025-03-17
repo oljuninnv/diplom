24000539a38eb62dcbd79569aa7439fd9071142f
Олюнин Никита <5608nik@mail.ru>
2025-03-18 03:09:51 +0700

    feat: сделал админ панель, локализацию на русский, сменил модель для отображения

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model
 {
@@ -12,4 +14,9 @@ class Role extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function Users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
 }
