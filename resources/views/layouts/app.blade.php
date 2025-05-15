0a7de95dbba4ffa82a73d3d7861dd95c23f34a3d
Олюнин Никита <5608nik@mail.ru>
2025-05-16 00:29:57 +0700

    fix: ссылка на заявки

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -133,7 +133,7 @@
                         @endif
                         @if (auth()->user()?->role->name === 'Admin')
                             <a href="/application"
-                                class="{{ request()->is('application*') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700' }} inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+                                class="{{ request()->is('application*') ? 'bg-indigo-50 border-indigo-500 text-indigo-700' : 'border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700' }} block pl-3 pr-4 py-2 border-l-4 text-base font-medium">
                                 Заявки
                             </a>
                             <a href="/admin"
